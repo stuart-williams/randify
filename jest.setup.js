c22a502b424caed242531d6a2841382900c16b39
Stuart Williams <stuart@tickx.co.uk>
2021-03-10 18:52:36 +0000

    fix unit tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,8 @@
 import "isomorphic-unfetch";
 import "@testing-library/jest-dom/extend-expect";
 
-require("dotenv").config();
+require("dotenv").config({
+  path: ".env.example",
+});
+
+process.env.API_URL = process.env.VERCEL_URL + "/api";
