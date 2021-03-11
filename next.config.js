7c09b8f96ba0aae8e3b3e107deab5243c3f8cbb5
Stuart Williams <stuart@tickx.co.uk>
2021-03-11 07:42:23 +0000

    fix api url

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+console.log(process.env.VERCEL_URL);
+
 module.exports = withBundleAnalyzer({
   env: {
-    API_URL: `//$${process.env.VERCEL_URL}/api`,
+    API_URL: `//${process.env.VERCEL_URL}/api`,
     REPO_URL: process.env.REPO_URL,
   },
   i18n: {
