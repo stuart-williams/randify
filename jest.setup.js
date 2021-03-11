9dd89fee58f6b7802dd35a05de1417fbbf00f4a9
Stuart Williams <stuart@tickx.co.uk>
2021-03-11 07:31:37 +0000

    env

--- jest.setup.js
+++ jest.setup.js
@@ -2,7 +2,7 @@ import "isomorphic-unfetch";
 import "@testing-library/jest-dom/extend-expect";
 
 require("dotenv").config({
-  path: ".env.example",
+  path: ".env.local.example",
 });
 
 process.env.API_URL = process.env.VERCEL_URL + "/api";
