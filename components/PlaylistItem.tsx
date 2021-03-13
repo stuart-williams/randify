2808618edeac23329fc3435d38612414c612ba2c
Stuart Williams <stuart@tickx.co.uk>
2021-03-13 10:13:46 +0000

    playlist item styles

--- components/PlaylistItem.tsx
+++ components/PlaylistItem.tsx
@@ -1,12 +1,4 @@
-import {
-  Flex,
-  Icon,
-  IconButton,
-  Link,
-  Text,
-  useColorModeValue,
-  VStack,
-} from "@chakra-ui/react";
+import { Flex, Icon, IconButton, Link } from "@chakra-ui/react";
 import useRandomise from "common/hooks/use-randomise";
 import { Playlist } from "common/spotify";
 import Image from "next/image";
@@ -18,37 +10,25 @@ export interface Props {
 }
 
 const PlaylistItem: FunctionComponent<Props> = ({ playlist }) => {
-  const { id, name, imageUrl, spotifyUrl, numberOfTracks } = playlist;
+  const { id, name, imageUrl, spotifyUrl } = playlist;
   const { randomise, isLoading } = useRandomise(playlist);
-  const subColor = useColorModeValue("blackAlpha.700", "whiteAlpha.700");
 
   return (
-    <Flex
-      data-cy={id}
-      alignItems="center"
-      sx={{
-        img: {
-          borderRadius: "md",
-        },
-      }}
-    >
-      <Flex position="relative" w={["40px", "50px"]} h={["40px", "50px"]}>
+    <Flex data-cy={id} alignItems="center">
+      <Flex position="relative" w="50px" h="50px">
         <Image src={imageUrl} alt={name} layout="fill" />
       </Flex>
-      <VStack mx={4} flex={1} spacing={0.5} alignItems="flex-start">
-        <Link
-          href={spotifyUrl}
-          isExternal
-          isTruncated
-          fontSize={["sm", "md"]}
-          fontWeight="600"
-        >
-          {name}
-        </Link>
-        <Text fontSize="xs" color={subColor}>
-          {numberOfTracks} Songs
-        </Text>
-      </VStack>
+      <Link
+        mx={4}
+        flex={1}
+        href={spotifyUrl}
+        isExternal
+        isTruncated
+        fontSize="md"
+        fontWeight="600"
+      >
+        {name}
+      </Link>
       <IconButton
         data-cy="randomise-btn"
         icon={<Icon as={RandomIcon} />}
