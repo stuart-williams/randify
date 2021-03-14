b7a2543eea8f28db174bfe73ceca9e1a3124a55c
Stuart Williams <stuart@tickx.co.uk>
2021-03-14 09:35:47 +0000

    first pass at pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({
+module.exports = withPlugins([withPWA, withBundleAnalyzer], {
   env: {
     API_URL: process.env.API_URL,
     REPO_URL: process.env.REPO_URL,
@@ -13,5 +15,9 @@ module.exports = withBundleAnalyzer({
   },
   images: {
     domains: ["mosaic.scdn.co", "i.scdn.co", "daily-mix.scdn.co"],
+  },
+  pwa: {
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
   },
 });
