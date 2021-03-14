0c1179034a5cadaa45e5f0aa463aff9b9a4a0f50
Stuart Williams <stuart@tickx.co.uk>
2021-03-14 10:01:08 +0000

    custom _document

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import React from "react";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
