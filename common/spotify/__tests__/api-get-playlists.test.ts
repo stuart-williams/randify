2d8439ce69099288e12ec92f03d2b6ffcf86182b
Stuart Williams <stuart@tickx.co.uk>
2021-03-14 07:56:19 +0000

    test recursive get playlists

--- common/spotify/__tests__/api-get-playlists.test.ts
+++ common/spotify/__tests__/api-get-playlists.test.ts
@@ -1,5 +1,3 @@
-// TODO: UPDATE TESTS!
-
 import { getPlaylists } from "common/spotify";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
@@ -14,101 +12,137 @@ const session: Session = {
   accessToken: "access_token_1",
 };
 
-const playlistsResponse: PartialDeep<SpotifyApi.ListOfCurrentUsersPlaylistsResponse> = {
-  items: [
-    {
-      id: "playlist_1",
-      name: "Playlist 1",
-      external_urls: {
-        spotify: "playlist/playlist_1",
-      },
-      images: [
-        {
-          url: "640.png",
-        },
-        {
-          url: "300.png",
-        },
-      ],
-      owner: {
-        display_name: "stuart",
-      },
-      tracks: {
-        total: 32,
-      },
-    },
-    {
-      id: "playlist_2",
-      name: "Playlist 2",
-      external_urls: {
-        spotify: "playlist/playlist_2",
-      },
-      images: [
-        {
-          url: "300.png",
-        },
-      ],
-      owner: {
-        display_name: "stuart",
-      },
-      tracks: {
-        total: 10,
-      },
-    },
-    {
-      id: "playlist_3",
-      name: "Not my Playlist",
-      external_urls: {
-        spotify: "playlist/playlist_3",
-      },
-      images: [
-        {
-          url: "640.png",
-        },
-      ],
-      owner: {
-        display_name: "someoneelse",
-      },
-      tracks: {
-        total: 10,
-      },
-    },
-    {
-      id: "playlist_4",
-      name: "Empty Playlist",
-      external_urls: {
-        spotify: "playlist/playlist_4",
-      },
-      images: [
-        {
-          url: "640.png",
-        },
-      ],
-      owner: {
-        display_name: "stuart",
-      },
-      tracks: {
-        total: 0,
-      },
-    },
-    {
-      id: "playlist_5",
-      name: "No Images Playlist",
-      external_urls: {
-        spotify: "playlist/playlist_5",
-      },
-      images: [],
-      owner: {
-        display_name: "stuart",
-      },
-      tracks: {
-        total: 10,
-      },
-    },
-  ],
-};
+interface RequestParams {
+  offset: string;
+  limit: string;
+}
 
 describe("getPlaylists", () => {
+  let params: RequestParams[] = [];
+  let responses: PartialDeep<SpotifyApi.ListOfCurrentUsersPlaylistsResponse>[] = [];
+
+  beforeEach(() => {
+    responses = [
+      {
+        items: [
+          {
+            id: "playlist_1",
+            name: "Playlist 1",
+            external_urls: {
+              spotify: "playlist/playlist_1",
+            },
+            images: [
+              {
+                url: "640.png",
+              },
+              {
+                url: "300.png",
+              },
+            ],
+            owner: {
+              display_name: "stuart",
+            },
+            tracks: {
+              total: 32,
+            },
+          },
+          {
+            id: "playlist_2",
+            name: "Playlist 2",
+            external_urls: {
+              spotify: "playlist/playlist_2",
+            },
+            images: [
+              {
+                url: "300.png",
+              },
+            ],
+            owner: {
+              display_name: "stuart",
+            },
+            tracks: {
+              total: 10,
+            },
+          },
+          {
+            id: "playlist_3",
+            name: "Not my Playlist",
+            external_urls: {
+              spotify: "playlist/playlist_3",
+            },
+            images: [
+              {
+                url: "640.png",
+              },
+            ],
+            owner: {
+              display_name: "someoneelse",
+            },
+            tracks: {
+              total: 10,
+            },
+          },
+          {
+            id: "playlist_4",
+            name: "Empty Playlist",
+            external_urls: {
+              spotify: "playlist/playlist_4",
+            },
+            images: [
+              {
+                url: "640.png",
+              },
+            ],
+            owner: {
+              display_name: "stuart",
+            },
+            tracks: {
+              total: 0,
+            },
+          },
+          {
+            id: "playlist_5",
+            name: "No Images Playlist",
+            external_urls: {
+              spotify: "playlist/playlist_5",
+            },
+            images: [],
+            owner: {
+              display_name: "stuart",
+            },
+            tracks: {
+              total: 10,
+            },
+          },
+        ],
+        total: 6,
+      },
+      {
+        items: [
+          {
+            id: "playlist_6",
+            name: "Playlist 6",
+            external_urls: {
+              spotify: "playlist/playlist_6",
+            },
+            images: [
+              {
+                url: "640.png",
+              },
+            ],
+            owner: {
+              display_name: "stuart",
+            },
+            tracks: {
+              total: 24,
+            },
+          },
+        ],
+        total: 6,
+      },
+    ];
+  });
+
   const server = setupServer(
     rest.get(`${process.env.SPOTIFY_API_URL}/me/playlists`, (req, res, ctx) => {
       // Assert required headers
@@ -118,7 +152,17 @@ describe("getPlaylists", () => {
         })
       );
 
-      return res(ctx.json(playlistsResponse));
+      const query = req.url.searchParams;
+
+      params = [
+        ...params,
+        {
+          offset: query.get("offset"),
+          limit: query.get("limit"),
+        } as RequestParams,
+      ];
+
+      return res(ctx.json(responses.shift()));
     })
   );
 
@@ -127,11 +171,11 @@ describe("getPlaylists", () => {
   afterAll(() => server.close());
 
   it("should return playlists as expected", async () => {
-    expect.assertions(2);
+    expect.assertions(4);
 
-    const playlists = await getPlaylists(session);
+    const response = await getPlaylists(session);
 
-    expect(playlists).toEqual([
+    expect(response).toEqual([
       {
         id: "playlist_1",
         name: "Playlist 1",
@@ -146,6 +190,18 @@ describe("getPlaylists", () => {
         spotifyUrl: "playlist/playlist_2",
         numberOfTracks: 10,
       },
+      {
+        id: "playlist_6",
+        name: "Playlist 6",
+        imageUrl: "640.png",
+        spotifyUrl: "playlist/playlist_6",
+        numberOfTracks: 24,
+      },
+    ]);
+
+    expect(params).toEqual([
+      { offset: "0", limit: "50" },
+      { offset: "4", limit: "50" },
     ]);
   });
 
