069feadd940a7b549bb89ef06566f75727d8003c
Stuart Williams <stuart@tickx.co.uk>
2021-03-14 13:24:48 +0000

    dark theme

--- common/theme.ts
+++ common/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 export default extendTheme({
   config: {
-    initialColorMode: "light",
+    initialColorMode: "dark",
     useSystemColorMode: false,
   },
   colors: {
@@ -17,6 +17,18 @@ export default extendTheme({
       700: "#096e30",
       800: "#00431b",
       900: "#001804",
+    },
+    gray: {
+      50: "#f2f2f2",
+      100: "#d9d9d9",
+      200: "#bfbfbf",
+      300: "#a6a6a6",
+      400: "#8c8c8c",
+      500: "#737373",
+      600: "#595959",
+      700: "#404040",
+      800: "#262626",
+      900: "#0d0d0d",
     },
   },
 });
--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -5,35 +5,30 @@ import {
   HStack,
   Icon,
   IconButton,
-  Link,
   Menu,
   MenuButton,
   MenuDivider,
   MenuItem,
   MenuList,
   Text,
-  useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
-import seo from "common/seo";
 import { signOut, useSession } from "next-auth/client";
 import React, { FunctionComponent } from "react";
 import {
-  FaGithub as GithubIcon,
-  FaMoon as MoonIcon,
   FaSignOutAlt as SignOutIcon,
-  FaSun as SunIcon,
   FaUserCircle as UserIcon,
 } from "react-icons/fa";
 import { useWindowScroll } from "react-use";
 
 const AppBar: FunctionComponent = () => {
-  const { toggleColorMode } = useColorMode();
   const bg = useColorModeValue("white", "gray.800");
-  const mode = useColorModeValue("Dark", "Light");
-  const ModeIcon = useColorModeValue(MoonIcon, SunIcon);
   const scroll = useWindowScroll();
   const [session] = useSession();
+
+  if (!session) {
+    return null;
+  }
 
   return (
     <Flex
@@ -43,49 +38,32 @@ const AppBar: FunctionComponent = () => {
       as="header"
       zIndex="sticky"
       position="sticky"
-      boxShadow={scroll.y > 0 ? "sm" : "none"}
+      boxShadow={scroll.y > 0 ? "md" : "none"}
     >
       <Container maxW="container.xl">
         <HStack spacing={1} justifyContent="flex-end">
-          <IconButton
-            as={Link}
-            icon={<Icon as={GithubIcon} />}
-            href={process.env.REPO_URL}
-            variant="ghost"
-            isExternal
-            aria-label={`Go to ${seo.title} Github page`}
-          />
-          {session && (
-            <Menu>
-              <MenuButton
-                as={IconButton}
-                icon={<Icon as={UserIcon} />}
-                data-cy="account-menu-btn"
-                variant="ghost"
-                aria-label="Account"
-              />
-              <MenuList>
-                <Box textAlign="center">
-                  <Text>{session.user.name}</Text>
-                </Box>
-                <MenuDivider />
-                <MenuItem
-                  icon={<Icon as={ModeIcon} />}
-                  data-cy="color-mode-btn"
-                  onClick={toggleColorMode}
-                >
-                  {mode} mode
-                </MenuItem>
-                <MenuItem
-                  icon={<SignOutIcon />}
-                  data-cy="signout-btn"
-                  onClick={() => signOut({ callbackUrl: "/signin" })}
-                >
-                  Sign out
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          )}
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              icon={<Icon as={UserIcon} />}
+              data-cy="account-menu-btn"
+              variant="ghost"
+              aria-label="Account"
+            />
+            <MenuList>
+              <Box textAlign="center">
+                <Text>{session.user.name}</Text>
+              </Box>
+              <MenuDivider />
+              <MenuItem
+                icon={<SignOutIcon />}
+                data-cy="signout-btn"
+                onClick={() => signOut({ callbackUrl: "/signin" })}
+              >
+                Sign out
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </HStack>
       </Container>
     </Flex>
